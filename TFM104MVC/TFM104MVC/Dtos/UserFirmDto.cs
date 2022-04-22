58cd941f25a1b6d723866bd6400dc7719549217e
Midomommy <tinasue165@gmail.com>
2022-04-22 23:03:03 +0800

    補充追蹤檔

---
+++ TFM104MVC/TFM104MVC/Dtos/UserFirmDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFM104MVC.Dtos
+{
+    public class UserFirmDto
+    {
+        public int Id { get; set; }
+
+        public string Account { get; set; }         //1.使用者帳號 信箱
+
+        public string Password { get; set; }        //2.使用者密碼 //SHA256
+
+        public string Salt { get; set; }    //加鹽
+
+        public string LastName { get; set; }  //姓
+
+        public string FirstName { get; set; }  //名
+
+        public string Phone { get; set; }
+
+        public bool Verification { get; set; }
+
+        public string RoleName { get; set; }
+
+        //廠商
+        public FirmDto Firms { get; set; }
+
+    }
+}
